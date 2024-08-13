0c0e5a857a68c72c407c2f5407288abcbbb88e40
Marco Antonio Gonzalez Ochoa <141543080+MAGO8a@users.noreply.github.com>
2024-08-12 22:57:23 -0400

    primeros pasos en el repo

---
+++ WebFer/src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <a-page-header
+      style="border: 1px solid rgb(235, 237, 240)"
+      title="Conceptos"
+      sub-title="Diseño, Construcción y Mobiliario "
+      @back="() => null"
+    />
+  </template>
+
+<style scoped>
+
+</style>
